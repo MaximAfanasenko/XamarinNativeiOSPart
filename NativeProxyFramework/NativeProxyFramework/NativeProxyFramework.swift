35674fb713087c596d8e2bd1dbdda301d7c7fe33
Maxim <32475421+MaximAfanasenko@users.noreply.github.com>
2022-03-14 16:21:09 +0700

    Update NativeProxyFramework.swift

--- NativeProxyFramework/NativeProxyFramework/NativeProxyFramework.swift
+++ NativeProxyFramework/NativeProxyFramework/NativeProxyFramework.swift
@@ -5,12 +5,14 @@ import UIKit
 @objc(NativeProxyFramework)
 public class NativeProxyFramework : NSObject {
     
+    //call example
     @objc
     public func pushSampleViewController(valueFromXamarinProject: String, navigationController: UINavigationController) {
         let nativeVC = SampleViewController(valueFromXamarinProject: valueFromXamarinProject)
         navigationController.pushViewController(nativeVC, animated: true)
     }
     
+    //example with example callback
     @objc
     public func pushSampleViewControllerWithCallback(valueFromXamarinProject: String, navigationController: UINavigationController, callback: (Int) -> Void) {
         let nativeVC = SampleViewController(valueFromXamarinProject: valueFromXamarinProject)
